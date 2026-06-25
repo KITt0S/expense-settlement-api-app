0c7ee35ffb24eb4f7c9d2e4bcc873d9c3ab8d289
KITt0S <rostys.gb@gmail.com>
2026-06-25 23:56:15 +0300

    feat: added Dockerfile

---
+++ src/main/java/com/k1ts/app/ExpenseSettlementApiApp.java
@@ -0,0 +1,12 @@
+package com.k1ts.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSettlementApiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseSettlementApiApp.class, args);
+    }
+}
